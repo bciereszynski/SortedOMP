d9c5d0a214027d79a301579fee0270766fcc84b9
bcier <b.ciereszynski@wp.pl>
2025-01-09 18:48:37 +0100

    add mergesort experiments to main

--- main.cpp
+++ main.cpp
@@ -5,8 +5,9 @@
 
 // ReSharper disable once CppUnusedIncludeDirective
 #include <omp.h>
-#include "quicksort.h"
 
+#include "mergesort.h"
+#include "quicksort.h"
 
 
 int main()
@@ -14,57 +15,114 @@ int main()
     constexpr size_t arraySize = 100000000;
     constexpr bool verbose = true;
 
-    int32_t* numbers = new int32_t[arraySize];
-    int32_t* numbers_serial = new int32_t[arraySize];
+    auto* numbers_serial_merge = new int32_t[arraySize];
+    auto* numbers_serial_quick = new int32_t[arraySize];
+    auto* numbers_merge = new int32_t[arraySize];
+    auto* numbers_quick = new int32_t[arraySize];
 
 
     // data generation
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<> dis(0, INT32_MAX);
-
-    for (std::size_t i = 0; i < arraySize; ++i) {
-        numbers[i] = dis(gen);
-    }
-    std::copy_n(numbers, arraySize, numbers_serial);
-
-    if (verbose) {
-        std::cout << "Starting quicksort parallel..." <<std::endl;
-    }
-    const auto start_parallel = std::chrono::high_resolution_clock::now();
-    #pragma omp parallel default(none) shared(numbers, arraySize) num_threads(6)
     {
-        #pragma omp single nowait
-        quicksort_parallel(numbers, 0, arraySize - 1);
-    }
-    auto end_parallel = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> elapsed_parallel = end_parallel - start_parallel;
-    const auto end_parallel = std::chrono::high_resolution_clock::now();
-    std::cout<< elapsed_parallel.count() << std::endl;
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_int_distribution<> dis(0, INT32_MAX);
 
-    if (verbose) {
-        std::cout << "Starting quicksort serial..." <<std::endl;
-    }
-
-    auto start_serial = std::chrono::high_resolution_clock::now();
-    quicksort_serial(numbers_serial, 0, arraySize - 1);
-    auto end_serial = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> elapsed_serial = end_serial - start_serial;
-    const auto start_serial = std::chrono::high_resolution_clock::now();
-    const auto end_serial = std::chrono::high_resolution_clock::now();
-    const std::chrono::duration<double> elapsed_serial = end_serial - start_serial;
-    std::cout<< elapsed_serial.count() << std::endl;
-
-    if (verbose) {
-        std::cout << "Output validation..." <<std::endl;
-    }
-    for (std::size_t i = 0; i < arraySize; ++i) {
-        if (numbers[i] != numbers_serial[i]) {
-            std::cout << "Error - " << i << ": " << numbers_serial[i] << " != " << numbers[i] << std::endl;
+        for (std::size_t i = 0; i < arraySize; ++i) {
+            const int32_t value = dis(gen);
+            numbers_serial_quick[i] = value;
+            numbers_serial_merge[i] = value;
+            numbers_quick[i] = value;
+            numbers_merge[i] = value;
         }
     }
 
-    delete[] numbers;
-    delete[] numbers_serial;
+    // quicksort parallel
+    {
+        if (verbose) {
+            std::cout << "Starting quicksort parallel..." <<std::endl;
+        }
+        const auto start_parallel = std::chrono::high_resolution_clock::now();
+        #pragma omp parallel default(none) shared(numbers_quick, arraySize) num_threads(6)
+        {
+        #pragma omp single nowait
+            quicksort_parallel(numbers_quick, 0, arraySize - 1);
+        }
+        const auto end_parallel = std::chrono::high_resolution_clock::now();
+        const std::chrono::duration<double> elapsed_parallel = end_parallel - start_parallel;
+        std::cout<< elapsed_parallel.count() << std::endl;
+    }
+
+    // quicksort serial
+    {
+        if (verbose) {
+            std::cout << "Starting quicksort serial..." <<std::endl;
+        }
+
+        const auto start_serial = std::chrono::high_resolution_clock::now();
+        quicksort_serial(numbers_serial_quick, 0, arraySize - 1);
+        const auto end_serial = std::chrono::high_resolution_clock::now();
+        const std::chrono::duration<double> elapsed_serial = end_serial - start_serial;
+        std::cout<< elapsed_serial.count() << std::endl;
+    }
+
+    // mergesort parallel
+    {
+        if (verbose) {
+            std::cout << "Starting mergesort parallel..." << std::endl;
+        }
+        const auto start_parallel = std::chrono::high_resolution_clock::now();
+        #pragma omp parallel default(none) shared(numbers_merge, arraySize) num_threads(6)
+        {
+        #pragma omp single nowait
+            mergesort_parallel(numbers_merge, 0, arraySize - 1);
+        }
+        const auto end_parallel = std::chrono::high_resolution_clock::now();
+        const std::chrono::duration<double> elapsed_parallel = end_parallel - start_parallel;
+        std::cout<< elapsed_parallel.count() << std::endl;
+    }
+
+    // mergesort serial
+    {
+        if (verbose) {
+            std::cout << "Starting mergesort serial..." <<std::endl;
+        }
+
+        const auto start_serial = std::chrono::high_resolution_clock::now();
+        mergesort_serial(numbers_serial_merge, 0, arraySize - 1);
+        const auto end_serial = std::chrono::high_resolution_clock::now();
+        const std::chrono::duration<double> elapsed_serial = end_serial - start_serial;
+        std::cout<< elapsed_serial.count() << std::endl;
+    }
+
+    // validation
+    {
+        if (verbose) {
+            std::cout << "Output validation..." <<std::endl;
+        }
+        auto end = false;
+        for (std::size_t i = 0; i < arraySize; ++i) {
+            if (numbers_serial_quick[i] != numbers_quick[i]) {
+                std::cout << "Error - quicksort! " << i << ": " << numbers_serial_quick[i] << " != " <<
+                    numbers_quick[i] << std::endl;
+                end = true;
+            }
+            if (numbers_serial_quick[i] != numbers_serial_merge[i]) {
+                std::cout << "Error - integration! " << i << ": " << numbers_serial_quick[i] << " != " <<
+                    numbers_serial_merge[i] << std::endl;
+                end = true;
+            }
+            if (numbers_serial_merge[i] != numbers_merge[i]) {
+                std::cout << "Error - mergesort! " << i << ": " << numbers_serial_merge[i] << " != " <<
+                    numbers_merge[i] << std::endl;
+                end = true;
+            }
+            if(end) {
+                break;
+            }
+        }
+    }
+
+    delete[] numbers_serial_quick;
+    delete[] numbers_quick;
     return 0;
 }
