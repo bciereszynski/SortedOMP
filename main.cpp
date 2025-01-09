19d8863e6a4d82f56a8ce586afbddc42ca0f598b
bcier <b.ciereszynski@wp.pl>
2025-01-09 17:54:26 +0100

    change calculations sequence

--- main.cpp
+++ main.cpp
@@ -24,16 +24,6 @@ int main()
     std::copy_n(numbers, arraySize, numbers_serial);
 
     if (verbose) {
-        std::cout << "Starting quicksort serial" <<std::endl;
-    }
-
-    auto start_serial = std::chrono::high_resolution_clock::now();
-    quicksort_serial(numbers_serial, 0, arraySize - 1);
-    auto end_serial = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> elapsed_serial = end_serial - start_serial;
-    std::cout<< elapsed_serial.count() << std::endl;
-
-    if (verbose) {
         std::cout << "Starting quicksort parallel" <<std::endl;
     }
     auto start_parallel = std::chrono::high_resolution_clock::now();
@@ -46,11 +36,22 @@ int main()
     std::chrono::duration<double> elapsed_parallel = end_parallel - start_parallel;
     std::cout<< elapsed_parallel.count() << std::endl;
 
+    if (verbose) {
+        std::cout << "Starting quicksort serial" <<std::endl;
+    }
+
+    auto start_serial = std::chrono::high_resolution_clock::now();
+    quicksort_serial(numbers_serial, 0, arraySize - 1);
+    auto end_serial = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed_serial = end_serial - start_serial;
+    std::cout<< elapsed_serial.count() << std::endl;
+
     for (std::size_t i = 0; i < arraySize; ++i) {
         if (numbers[i] != numbers_serial[i]) {
             std::cout << "Error - " << i << ": " << numbers_serial[i] << " != " << numbers[i] << std::endl;
         }
     }
+
     delete[] numbers;
     delete[] numbers_serial;
     return 0;
