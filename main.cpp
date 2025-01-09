207b8413f7d373fada3fd151ed012f8adef6cea3
bcier <b.ciereszynski@wp.pl>
2025-01-09 13:36:30 +0100

    add time measure and serial run

--- main.cpp
+++ main.cpp
@@ -11,12 +11,14 @@ int main()
     const size_t arraySize = 100000000;
     const bool verbose = false;
     int32_t* numbers = new int32_t[arraySize];
+    int32_t* numbers_serial = new int32_t[arraySize];
 
     std::srand(std::time(nullptr));
 
     for (std::size_t i = 0; i < arraySize; ++i) {
         numbers[i] = std::rand();
     }
+    std::copy_n(numbers, arraySize, numbers_serial);
 
     if (verbose) {
         std::cout << "Generated array:" << std::endl;
@@ -25,14 +27,31 @@ int main()
         }
     }
 
-    quicksort_parallel(numbers, 0, arraySize - 1);
+    auto start_serial = std::chrono::high_resolution_clock::now();
+    quicksort_serial(numbers_serial, 0, arraySize - 1);
+    auto end_serial = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed_serial = end_serial - start_serial;
+    std::cout<< elapsed_serial.count() << std::endl;
 
-    if(verbose) {
-        std::cout << "Sorted array:" << std::endl;
-        for (std::size_t i = 0; i < arraySize; ++i) {
-            std::cout << i << ": " << numbers[i] << std::endl;
+    if (verbose) {
+        std::cout << "Starting quicksort parallel" <<std::endl;
+    }
+    auto start_parallel = std::chrono::high_resolution_clock::now();
+    #pragma omp parallel default(none) shared(numbers, arraySize)
+    {
+        #pragma omp single nowait
+        quicksort_parallel(numbers, 0, arraySize - 1);
+    }
+    auto end_parallel = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed_parallel = end_parallel - start_parallel;
+    std::cout<< elapsed_parallel.count() << std::endl;
+
+    for (std::size_t i = 0; i < arraySize; ++i) {
+        if (numbers[i] != numbers_serial[i]) {
+            std::cout << "Error - " << i << ": " << numbers_serial[i] << " != " << numbers[i] << std::endl;
         }
     }
     delete[] numbers;
+    delete[] numbers_serial;
     return 0;
 }
--- quicksort.h
+++ quicksort.h
@@ -5,4 +5,5 @@
 #ifndef QUICKSORT_H
 #define QUICKSORT_H
 void quicksort_parallel(int * __restrict__ A, long low, long high, long cutoff = 10000);
+void quicksort_serial(int * __restrict__ A, long low, long high);
 #endif //QUICKSORT_H
